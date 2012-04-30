a7605f8370bdea3b23853a6cd342a6b34b5ba57b
Ryan Bigg <radarlistener@gmail.com>
2012-04-30 16:16:32 -0500

    Update full_checkout_spec to cover paying by check and credit card

--- core/spec/requests/full_checkout_spec.rb
+++ core/spec/requests/full_checkout_spec.rb
@@ -1,35 +1,50 @@
 require 'spec_helper'
 
-describe "Full Checkout flow" do
+describe "Full Checkout flow", :js => true do
   before do
     shipping_method = Factory(:shipping_method, :zone => Spree::Zone.find_by_name("North America"))
     shipping_method.calculator.set_preference(:amount, 10)
 
     Factory(:payment_method, :environment => 'test')
     Factory(:product, :name => "RoR Mug")
+    Factory(:authorize_net_payment_method, :environment => 'test')
   end
 
   let!(:address) { Factory(:address, :state => Spree::State.first) }
 
-  it "can go through the whole checkout process", :js => true do
-    visit spree.root_path
-    click_link "RoR Mug"
-    click_button "Add To Cart"
-    click_link "Checkout"
+  context "can go through the whole checkout process" do
+    before do
+      visit spree.root_path
+      click_link "RoR Mug"
+      click_button "Add To Cart"
+      click_link "Checkout"
 
-    page.should have_content("Billing Address")
-    page.should have_content("Shipping Address")
+      page.should have_content("Billing Address")
+      page.should have_content("Shipping Address")
 
-    str_addr = "bill_address"
-    select "United States", :from => "order_#{str_addr}_attributes_country_id"
-    ['firstname', 'lastname', 'address1', 'city', 'zipcode', 'phone'].each do |field|
-      fill_in "order_#{str_addr}_attributes_#{field}", :with => "#{address.send(field)}"
+      str_addr = "bill_address"
+      select "United States", :from => "order_#{str_addr}_attributes_country_id"
+      ['firstname', 'lastname', 'address1', 'city', 'zipcode', 'phone'].each do |field|
+        fill_in "order_#{str_addr}_attributes_#{field}", :with => "#{address.send(field)}"
+      end
+      select "#{address.state.name}", :from => "order_#{str_addr}_attributes_state_id"
+      check "order_use_billing"
+      click_button "Save and Continue"
+      click_button "Save and Continue"
+      click_button "Save and Continue"
     end
-    select "#{address.state.name}", :from => "order_#{str_addr}_attributes_state_id"
-    check "order_use_billing"
-    click_button "Save and Continue"
-    click_button "Save and Continue"
-    click_button "Save and Continue"
-    page.should have_content("Your order has been processed successfully")
+
+    it "paying by check" do
+      click_button "Save and Continue"
+      page.should have_content("Your order has been processed successfully")
+    end
+
+    it "paying by card" do
+      choose 'Credit Card'
+      fill_in "card_number", :with => "4111111111111111"
+      fill_in "card_code", :with => "123"
+      click_button "Save and Continue"
+      page.should have_content("Your order has been processed successfully")
+    end
   end
 end
